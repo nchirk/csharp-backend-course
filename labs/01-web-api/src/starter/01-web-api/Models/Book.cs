bae89ab94bca1f68f1402336bbc2063fa64c6847
Natalia Chirkova <nchirk@gmail.com>
2026-09-25 17:46:36 +0400

    Add lab-1

---
+++ labs/01-web-api/src/starter/01-web-api/Models/Book.cs
@@ -0,0 +1,9 @@
+namespace _01_web_api.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public int Year { get; set; }
+}
